9461a47f24c5c210e95cf75a755ae736c2741321
Ralph van Vlijmen <dragstylernl@gmail.com>
2023-01-02 17:46:29 +0100

    rewritten removing mutables into return types
    gotta have a look at focusing on launched programs, they now seem to launch in the background

--- PCM/src/main.rs
+++ PCM/src/main.rs
@@ -5,7 +5,6 @@ use std::fmt::Write;
 use toml;
 use serde_derive::Deserialize;
 use std::fs;
-use std::process::exit;
 
 const COMMAND_KEY: &str = "PCM";
 
@@ -15,53 +14,53 @@ struct Commands {
     path: String,
 }
 
+struct Input {
+    input: String,
+    old_key: Vec<Keycode>,
+}
+
 fn main() -> anyhow::Result <()> {
     
     let device_state = DeviceState::new();
-    let mut old_key = device_state.get_keys();
-    let mut input = String::new();
 
-    let contents = match fs::read_to_string("datafiles/cmd_and_path.toml") {
-        Ok(c) => c,
-        Err(_) => {
-            exit(1);
-        }
-    };
-    let data_table: HashMap<String, Vec<Commands>> = toml::from_str(&contents).unwrap();
+    let mut keyboard:Input = Input { input: String::new(), old_key: device_state.get_keys() };
+   
+    let contents = fs::read_to_string("datafiles/cmd_and_path.toml")?;
+    let data_table: HashMap<String, Vec<Commands>> = toml::from_str(&contents)?;
     let data:&[Commands] = &data_table["commands"];
 
     // MAIN LOOP =======
     loop {
-        read_input(&device_state, &mut old_key, &mut input);
+        keyboard = read_input(&device_state, &keyboard);
 
-        if input.contains(COMMAND_KEY) {
-            
-            read_lauchables(&data, &mut input);
-            
+        if keyboard.input.contains(COMMAND_KEY) {
+            keyboard.input = read_lauchables(&data, &keyboard.input);   
         }
     }
 }
 
-fn read_input(device_state:&DeviceState, old_key:&mut Vec<Keycode>, input:&mut String) {
-    let keys = device_state.get_keys();
-    if *old_key != keys {
-        for key in keys.iter(){
+fn read_input(device_state:&DeviceState, keyboard:&Input) -> Input {
+    let mut current_keyboard:Input = Input { input: keyboard.input.to_string(), old_key: device_state.get_keys() };
+    if keyboard.old_key != current_keyboard.old_key {
+        for key in current_keyboard.old_key.iter(){
             match key {
-                Keycode::Enter | Keycode::Space | Keycode::LShift => { *input = String::new(); },
+                Keycode::Enter | Keycode::Space | Keycode::LShift => { current_keyboard.input = String::new(); },
                 _ => {
-                    write!(*input, "{:?}", key);
+                    write!(current_keyboard.input, "{:?}", key);
                 }
             }
         }
-        *old_key = keys;
     }
+    return current_keyboard;
 }
 
-fn read_lauchables(data:&[Commands], input:&mut String){
+fn read_lauchables(data:&[Commands], input:&String) -> String{
+    let mut r_string = input.to_string();
     for i in 0..data.len() {
         if input.contains(&data[i].command) {
             let _ = Command::new(&data[i].path).spawn();
-            *input = String::new();
+            r_string = String::new();
         }
     }
+    return r_string;
 }
