e3e889d192acdcf3b32601851f5b63ad3e365513
Ralph van Vlijmen <dragstylernl@gmail.com>
2023-01-02 17:48:25 +0100

    re-format

--- PCM/src/main.rs
+++ PCM/src/main.rs
@@ -1,10 +1,10 @@
 use device_query::{DeviceQuery, DeviceState, Keycode};
-use std::collections::HashMap;
-use std::process::Command;
-use std::fmt::Write;
-use toml;
 use serde_derive::Deserialize;
+use std::collections::HashMap;
+use std::fmt::Write;
 use std::fs;
+use std::process::Command;
+use toml;
 
 const COMMAND_KEY: &str = "PCM";
 
@@ -19,32 +19,39 @@ struct Input {
     old_key: Vec<Keycode>,
 }
 
-fn main() -> anyhow::Result <()> {
-    
+fn main() -> anyhow::Result<()> {
     let device_state = DeviceState::new();
 
-    let mut keyboard:Input = Input { input: String::new(), old_key: device_state.get_keys() };
-   
+    let mut keyboard: Input = Input {
+        input: String::new(),
+        old_key: device_state.get_keys(),
+    };
+
     let contents = fs::read_to_string("datafiles/cmd_and_path.toml")?;
     let data_table: HashMap<String, Vec<Commands>> = toml::from_str(&contents)?;
-    let data:&[Commands] = &data_table["commands"];
+    let data: &[Commands] = &data_table["commands"];
 
     // MAIN LOOP =======
     loop {
         keyboard = read_input(&device_state, &keyboard);
 
         if keyboard.input.contains(COMMAND_KEY) {
-            keyboard.input = read_lauchables(&data, &keyboard.input);   
+            keyboard.input = read_lauchables(&data, &keyboard.input);
         }
     }
 }
 
-fn read_input(device_state:&DeviceState, keyboard:&Input) -> Input {
-    let mut current_keyboard:Input = Input { input: keyboard.input.to_string(), old_key: device_state.get_keys() };
+fn read_input(device_state: &DeviceState, keyboard: &Input) -> Input {
+    let mut current_keyboard: Input = Input {
+        input: keyboard.input.to_string(),
+        old_key: device_state.get_keys(),
+    };
     if keyboard.old_key != current_keyboard.old_key {
-        for key in current_keyboard.old_key.iter(){
+        for key in current_keyboard.old_key.iter() {
             match key {
-                Keycode::Enter | Keycode::Space | Keycode::LShift => { current_keyboard.input = String::new(); },
+                Keycode::Enter | Keycode::Space | Keycode::LShift => {
+                    current_keyboard.input = String::new();
+                }
                 _ => {
                     write!(current_keyboard.input, "{:?}", key);
                 }
@@ -54,7 +61,7 @@ fn read_input(device_state:&DeviceState, keyboard:&Input) -> Input {
     return current_keyboard;
 }
 
-fn read_lauchables(data:&[Commands], input:&String) -> String{
+fn read_lauchables(data: &[Commands], input: &String) -> String {
     let mut r_string = input.to_string();
     for i in 0..data.len() {
         if input.contains(&data[i].command) {
